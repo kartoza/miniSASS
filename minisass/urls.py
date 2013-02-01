128762a883a90cd9f5282952528444604d88d220
Jurgen Blignaut <jurgen@webtide.co.za>
2013-02-01 17:27:24 +0200

    Adding django-cms basic project structure for the site

---
+++ minisass/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'minisass.views.home', name='home'),
+    # url(r'^minisass/', include('minisass.foo.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    # django-cms urls
+    url(r'^', include('cms.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+            url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+            url(r'', include('django.contrib.staticfiles.urls')),
+            ) + urlpatterns
