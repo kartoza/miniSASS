ebe9805fc6d5e5b815a65ffb2dda1c4573bccb20
Jurgen Blignaut <jurgen@webtide.co.za>
2013-05-29 21:14:57 +0200

    Fixed multilingual url confs for cms

--- minisass/urls.py
+++ minisass/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import *
+from django.conf import settings
+
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from cms.sitemaps import CMSSitemap
 from cmsplugin_blog.sitemaps import BlogSitemap
@@ -10,6 +13,13 @@ from cmsplugin_blog.sitemaps import BlogSitemap
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
+)
+
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += i18n_patterns('',
     # Examples:
     # url(r'^$', 'minisass.views.home', name='home'),
     # url(r'^minisass/', include('minisass.foo.urls')),
@@ -39,5 +49,5 @@ if settings.DEBUG:
     urlpatterns = patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        url(r'', include('django.contrib.staticfiles.urls')),
+        # url(r'', include('django.contrib.staticfiles.urls')),
     ) + urlpatterns
