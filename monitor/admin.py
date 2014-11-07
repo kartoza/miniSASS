ccc20a04d94526d529e68f669200f1586701f72c
Ismail Sunni <imajimatika@gmail.com>
2014-11-07 09:28:30 +0700

    [Fix #14] Add filter and bulk actions for observations page in django-admin.

--- monitor/admin.py
+++ monitor/admin.py
@@ -3,5 +3,30 @@ from django.contrib import admin
 from monitor.models import Sites
 from monitor.models import Observations
 
+
+def make_verified(modeladmin, request, queryset):
+    queryset.update(flag='clean')
+make_verified.short_description = (
+    "Mark selected observations as verified (clean)")
+
+
+def make_unverified(modeladmin, request, queryset):
+    queryset.update(flag='dirty')
+make_unverified.short_description = (
+    "Mark selected observations as unverified (dirty)")
+
+
+class ObservationsAdmin(admin.ModelAdmin):
+    list_display = (
+        'gid',
+        'user',
+        'site',
+        'obs_date',
+        'score',
+        'flag')
+    list_filter = ('flag',)
+    actions = [make_verified, make_unverified]
+
+
 admin.site.register(Sites)
-admin.site.register(Observations)
+admin.site.register(Observations, ObservationsAdmin)
