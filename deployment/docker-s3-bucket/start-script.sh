49cc918b5595dc6fb8cbf664a0d1639f27dca6a5
mazano <addloe@gmail.com>
2023-10-11 16:32:00 +0200

    add pg_featserv and mounting s3 bucket

---
+++ deployment/docker-s3-bucket/start-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo $AWS_ACCESS_KEY:$AWS_SECRET_ACCESS_KEY > /root/.passwd-s3fs && \
+chmod 600 /root/.passwd-s3fs
+
+mkdir -p ${S3_MOUNT_DIRECTORY}
+
+
+echo "s3fs ${S3_BUCKET_NAME} ${S3_MOUNT_DIRECTORY} -o passwd_file=/root/.passwd-s3fs -o url=${MINIO_URL} -o use_path_request_style"
+command="s3fs ${S3_BUCKET_NAME} ${S3_MOUNT_DIRECTORY} -o passwd_file=/root/.passwd-s3fs -o url=${MINIO_URL} -o dbglevel=info -f -o curldbg -o use_path_request_style"
+eval $command
+
+
+
+
