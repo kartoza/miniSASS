5c932f8e8d9e590a027e3ae6d8b34d3222ebdcdb
mazano <addloe@gmail.com>
2023-10-11 16:37:03 +0200

    remove echo command in mount folder

--- deployment/docker-s3-bucket/start-script.sh
+++ deployment/docker-s3-bucket/start-script.sh
@@ -5,8 +5,6 @@ chmod 600 /root/.passwd-s3fs
 
 mkdir -p ${S3_MOUNT_DIRECTORY}
 
-
-echo "s3fs ${S3_BUCKET_NAME} ${S3_MOUNT_DIRECTORY} -o passwd_file=/root/.passwd-s3fs -o url=${MINIO_URL} -o use_path_request_style"
 command="s3fs ${S3_BUCKET_NAME} ${S3_MOUNT_DIRECTORY} -o passwd_file=/root/.passwd-s3fs -o url=${MINIO_URL} -o dbglevel=info -f -o curldbg -o use_path_request_style"
 eval $command
 
