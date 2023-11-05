a73ab5c06993603e1590aa3330d1899d948daf4a
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-05 21:43:00 +0200

    patch: update urls

--- django_project/minisass/urls.py
+++ django_project/minisass/urls.py
@@ -1,60 +1,44 @@
-from django.conf import settings
-
-from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+"""
+URL configuration for core project.
+"""
 from django.contrib import admin
-from django.conf.urls.i18n import i18n_patterns
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.views.static import serve
+from django.views.generic import TemplateView
 from cms.sitemaps import CMSSitemap
 from cmsplugin_blog.sitemaps import BlogSitemap
-from django.views.generic import TemplateView
-from minisass_frontend.views import ObservationsView
+
+from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
-)
 
-
-urlpatterns += staticfiles_urlpatterns()
-
-urlpatterns += i18n_patterns('',
-    # Examples:
-    # url(r'^$', 'minisass.views.home', name='home'),
-    # url(r'^minisass/', include('minisass.foo.urls')),
-    (r'^tinymce/', include('tinymce.urls')),
-    # (r'^monitor/', include('monitor.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-
-    # registration urls
-    url(r'^autocomplete/', include('minisass_registration.urls')),
-    url(r'^accounts/', include('minisass_registration.backends.urls')),
-
-    # Main frontend URL
-    url(r'^$', TemplateView.as_view(template_name="react_base.html"), name="home"),
-
-    url(r'^api/observations/$', ObservationsView.as_view(), name='observations-api'),
-
-    # CMS URL
-    url(r'^cms/', include('cms.urls')),
-
-    # site map
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {
+urlpatterns = [
+    path('jsi18n/<str:packages>/', 'django.views.i18n.JavascriptCatalog.as_view', name='javascript-catalog'),
+    path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
+    path('sitemap.xml', 'django.contrib.sitemaps.views.sitemap', {
         'sitemaps': {
             'cmspages': CMSSitemap,
             'blogentries': BlogSitemap
         }
     }),
-)
+    # Serve static files
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+
+    # Add a new URL pattern for /mainPage
+    path("home/", TemplateView.as_view(template_name="base.html"), name="home"),
+
+     # Include the URLs of the "myapp" app
+    path('authentication/', include('minisass_authentication.urls')),
+
+    # cms urls
+    path('cms/', include('cms.urls')),
+]
 
 if settings.DEBUG:
-    urlpatterns = patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        # url(r'', include('django.contrib.staticfiles.urls')),
-    ) + urlpatterns
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
