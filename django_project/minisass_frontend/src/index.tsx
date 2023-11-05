52ece44bfb6544942ddf3cc9e762a9843bfe6224
tinashe <tinashe@kartoza.com>
2023-11-05 23:47:38 +0200

    update import path

--- django_project/minisass_frontend/src/index.tsx
+++ django_project/minisass_frontend/src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.css";
 import "./styles/tailwind.css";
-import { AuthProvider } from 'AuthContext';
+import { AuthProvider } from './AuthContext';
 
 ReactDOM.render(
   <AuthProvider>
