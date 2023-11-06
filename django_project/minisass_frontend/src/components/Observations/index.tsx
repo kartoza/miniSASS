643bc7f4c5af9eea3cfe910a4c188effc151b4b6
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-06 16:04:08 +0200

    patch: set title color based on score

--- django_project/minisass_frontend/src/components/Observations/index.tsx
+++ django_project/minisass_frontend/src/components/Observations/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import { Img, Text } from "../../components";
 import "react-circular-progressbar/dist/styles.css";
@@ -37,18 +37,41 @@ type DesktopTwoColumnscoreProps = Omit<
   const newURL = baseUrl + '/' + replacementPath;
 
 const Observations: React.FC<DesktopTwoColumnscoreProps> = (props) => {
-  const isRedProgressBar = parseFloat(props.score1 || "0") < 6;
-  const progressBarColor = isRedProgressBar ? "red" : "green";
-  const titleColor = isRedProgressBar ? "text-red-600" : "text-green-800";
-  const renderCrab = isRedProgressBar
-    ? `${newURL}img_image2_24x30.png`
-    : `${newURL}img_image2.png`;
+  const [isRedProgressBar, setIsRedProgressBar] = useState<boolean>(false);
+  const [titleColor, setTitleColor] = useState<string>('');
+  const [progressBarColor, setProgressBarColor] = useState<string>('');
+  const [renderCrab, setRenderCrab] = useState<string>('');
+  
+
+  useEffect(() => {
+    if(parseFloat(props.score || "0") < 6){
+      setIsRedProgressBar(true)
+      setTitleColor("text-red-600")
+      setProgressBarColor("red")
+      setRenderCrab(`${newURL}img_image2_24x30.png`)
+    }else {
+      console.log('score more')
+      setIsRedProgressBar(false)
+      setTitleColor("text-green-800")
+      setProgressBarColor("green")
+      setRenderCrab(`${newURL}img_image2.png`)
+    }
+      
+
+  }, [props.score]);
+
+  // const isRedProgressBar = parseFloat(props.score1 || "0") < 6;
+  // const progressBarColor = isRedProgressBar ? "red" : "green";
+  // const titleColor = isRedProgressBar ? "text-red-600" : "text-green-800";
+  // const renderCrab = isRedProgressBar
+  //   ? `${newURL}img_image2_24x30.png`
+  //   : `${newURL}img_image2.png`;
 
   return (
     <div className={props.className}>
       <div className="flex flex-col gap-2 items-start justify-start w-full">
         <Text
-          className={`text-lg w-full ${titleColor}`}
+          className={`${titleColor} text-lg w-full`}
           size="txtRalewayBold18Green800"
         >
           {props?.username}
