d8ac7930a5a8379f3353d126110ab38ee08b7aed
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-06 21:27:35 +0200

    patch: update urls

--- django_project/minisass/urls.py
+++ django_project/minisass/urls.py
@@ -1,40 +1,35 @@
-"""
-URL configuration for core project.
-"""
 from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.views.static import serve
 from django.views.generic import TemplateView
-from cms.sitemaps import CMSSitemap
-from cmsplugin_blog.sitemaps import BlogSitemap
-
-from django.contrib import admin
-
-admin.autodiscover()
-
+# from cms.sitemaps import CMSSitemap
+# from cmsplugin_blog.sitemaps import BlogSitemap
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
-    path('jsi18n/<str:packages>/', 'django.views.i18n.JavascriptCatalog.as_view', name='javascript-catalog'),
+    path('jsi18n/<str:packages>/', JavaScriptCatalog.as_view(), name='javascript-catalog'),  # Use JavaScriptCatalog directly
     path('admin/', admin.site.urls),
-    path('tinymce/', include('tinymce.urls')),
-    path('sitemap.xml', 'django.contrib.sitemaps.views.sitemap', {
-        'sitemaps': {
-            'cmspages': CMSSitemap,
-            'blogentries': BlogSitemap
-        }
-    }),
+    
+    # path('tinymce/', include('tinymce.urls')),
+    # path('sitemap.xml', django.contrib.sitemaps.views.sitemap, {
+    #     'sitemaps': {
+    #         'cmspages': CMSSitemap,
+    #         'blogentries': BlogSitemap
+    #     }
+    # }),
+    
     # Serve static files
     re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 
     # Add a new URL pattern for /mainPage
     path("home/", TemplateView.as_view(template_name="base.html"), name="home"),
 
-     # Include the URLs of the "myapp" app
+    # Include the URLs of the "myapp" app
     path('authentication/', include('minisass_authentication.urls')),
 
     # cms urls
-    path('cms/', include('cms.urls')),
+    # path('cms/', include('cms.urls')),
 ]
 
 if settings.DEBUG:
