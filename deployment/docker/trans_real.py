afda1700e31eac888814f333940c554120f58ba5
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-07 12:11:45 +0200

    patch: update imports

--- deployment/docker/trans_real.py
+++ deployment/docker/trans_real.py
@@ -13,7 +13,7 @@ try:
 except ImportError:
     from io import StringIO  # Use io.StringIO in Python 3
 
-from django.utils.importlib import import_module
+from importlib import import_module
 from django.utils.safestring import mark_safe, SafeData
 
 
