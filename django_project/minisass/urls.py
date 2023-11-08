dfe99d155dbf10fc1648471557d2e1fa07be4a1d
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-08 12:58:30 +0200

    patch: update paths

--- django_project/minisass/urls.py
+++ django_project/minisass/urls.py
@@ -3,33 +3,22 @@ from django.urls import path, re_path, include
 from django.conf import settings
 from django.views.static import serve
 from django.views.generic import TemplateView
-# from cms.sitemaps import CMSSitemap
-# from cmsplugin_blog.sitemaps import BlogSitemap
 from django.views.i18n import JavaScriptCatalog
+
+admin.autodiscover()
 
 urlpatterns = [
     path('jsi18n/<str:packages>/', JavaScriptCatalog.as_view(), name='javascript-catalog'),  # Use JavaScriptCatalog directly
     path('admin/', admin.site.urls),
     
-    # path('tinymce/', include('tinymce.urls')),
-    # path('sitemap.xml', django.contrib.sitemaps.views.sitemap, {
-    #     'sitemaps': {
-    #         'cmspages': CMSSitemap,
-    #         'blogentries': BlogSitemap
-    #     }
-    # }),
-    
     # Serve static files
     re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 
-    # Add a new URL pattern for /mainPage
-    path("home/", TemplateView.as_view(template_name="base.html"), name="home"),
+    # Make the home path the default URL
+    path("", TemplateView.as_view(template_name="react_base.html"), name="home"),
 
-    # Include the URLs of the "myapp" app
+    # Include authentication URLs
     path('authentication/', include('minisass_authentication.urls')),
-
-    # cms urls
-    # path('cms/', include('cms.urls')),
 ]
 
 if settings.DEBUG:
