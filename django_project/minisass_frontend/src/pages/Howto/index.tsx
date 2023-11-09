c58e6f3b73bd738d02d076c3632bc9cf125db15f
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-09 19:30:23 +0200

    patch: make buttons same length

--- django_project/minisass_frontend/src/pages/Howto/index.tsx
+++ django_project/minisass_frontend/src/pages/Howto/index.tsx
@@ -120,9 +120,9 @@ const HowtoPage: React.FC = () => {
                   smooth={true}
                   duration={800}  // Set the duration of the scrolling animation
                 >
-                <div className="h-28 ml-auto my-auto w-[95%]">
-                  <div className="bg-blue_gray-500 h-28 ml-auto my-auto rounded-bl-[25px] rounded-br-[25px] rounded-tr-[25px] w-full"></div>
-                  <div className="absolute bottom-[10%] flex flex-col h-max inset-y-[0] items-center justify-start my-auto right-[15%] w-[64%]">
+                <div className="flex h-24 md:h-28 justify-end mt-auto mx-auto w-full">
+                  <div className="bg-blue_gray-500 sm:bottom-[] h-28 mt-auto mx-auto relative rounded-bl-[25px] rounded-br-[25px] rounded-tr-[25px] sm:top-[] w-full"></div>
+                    <div className="absolute bottom-[45%] flex flex-col inset-x-[0] items-center justify-start mx-auto w-[51%]">
                     
                     <Text
                       className="mt-1 text-center text-sm text-white-A700 tracking-[0.98px] uppercase w-auto"
