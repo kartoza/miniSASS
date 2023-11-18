38f92c077820ab291dabfc3188204d3560cbdcd1
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-18 09:00:43 +0200

    feat: add observation details to sidebar component

--- django_project/minisass_frontend/src/components/Sidebar/index.tsx
+++ django_project/minisass_frontend/src/components/Sidebar/index.tsx
@@ -1,19 +1,32 @@
+// Sidebar.tsx
+
 import React from "react";
 import DataInputForm from "../../components/DataInputForm";
+import ObservationDetails from "../../components/ObservationDetails";
 
 interface SidebarProps {
   isOpen: boolean;
+  isObservationDetails: boolean;
+  setSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
+const Sidebar: React.FC<SidebarProps> = ({ isOpen, isObservationDetails, setSidebarOpen }) => {
   return (
     <div
       className={`absolute ${
         isOpen ? "left-0" : "-left-full"
-      } bg-white-A700 flex flex-col gap-2 items-start justify-center px-[18px] py-5 rounded-bl-[10px] rounded-br-[10px] rounded-tr-[10px] top-[1%] sm:top-[25px] w-auto transition-all duration-300`}
+      } bg-white-A700 flex flex-col  items-start justify-center py-5 rounded-bl-[10px] rounded-br-[10px] rounded-tr-[10px] top-[1%] sm:top-[25px] w-auto transition-all duration-300`}
     >
-      <DataInputForm className="bg-white-A700 flex flex-col gap-6 items-start justify-start pb-3 px-3 rounded-bl-[10px] rounded-br-[10px] rounded-tr-[10px] shadow-bs w-full" />
-     
+      {isObservationDetails ? (
+        <ObservationDetails 
+          classname="bg-white-A700 flex flex-col gap-6 items-start justify-start pb-3 px-3 rounded-bl-[10px] rounded-br-[10px] rounded-tr-[10px] shadow-bs w-full" 
+          observation_id={`1`}
+          setSidebarOpen={setSidebarOpen}
+        />
+      ):(
+        <DataInputForm className="bg-white-A700 flex flex-col gap-6 items-start justify-start pb-3 px-3 rounded-bl-[10px] rounded-br-[10px] rounded-tr-[10px] shadow-bs w-full" />
+      )}
+      
     </div>
   );
 };
