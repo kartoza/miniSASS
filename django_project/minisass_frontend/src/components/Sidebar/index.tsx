8b3a9e08bfc17dbaa18e66e09d07c5c4571bd1b4
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-18 08:47:27 +0200

    patch: remove the extra whitespace on sidebar

---
+++ django_project/minisass_frontend/src/components/Sidebar/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import DataInputForm from "../../components/DataInputForm";
+
+interface SidebarProps {
+  isOpen: boolean;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
+  return (
+    <div
+      className={`absolute ${
+        isOpen ? "left-0" : "-left-full"
+      } bg-white-A700 flex flex-col  items-start justify-center py-5 rounded-bl-[10px] rounded-br-[10px] rounded-tr-[10px] top-[1%] sm:top-[25px] w-auto transition-all duration-300`}
+    >
+      <DataInputForm className="bg-white-A700 flex flex-col gap-6 items-start justify-start pb-3 px-3 rounded-bl-[10px] rounded-br-[10px] rounded-tr-[10px] shadow-bs w-full" />
+     
+    </div>
+  );
+};
+
+export default Sidebar;
