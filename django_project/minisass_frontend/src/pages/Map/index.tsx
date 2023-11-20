ff7b4c59e22104c1278f915a8a4bcf4a16ec2cc3
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-20 18:29:20 +0200

    patch: add use ref import

--- django_project/minisass_frontend/src/pages/Map/index.tsx
+++ django_project/minisass_frontend/src/pages/Map/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import { Button, Img, Input, Text } from "../../components";
