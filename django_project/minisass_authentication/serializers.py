19b64491010b2dad234db2e99a5898b778f13275
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-20 16:31:58 +0200

    patch: update serializer

--- django_project/minisass_authentication/serializers.py
+++ django_project/minisass_authentication/serializers.py
@@ -5,7 +5,12 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class PasswordResetRequestSerializer(serializers.Serializer):
