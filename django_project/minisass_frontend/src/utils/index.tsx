81458496545584217db2a72936bb6608032899ed
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-23 00:13:04 +0200

    feat: define global variables to use in any component

--- django_project/minisass_frontend/src/utils/index.tsx
+++ django_project/minisass_frontend/src/utils/index.tsx
@@ -1,3 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+import React from 'react';
+
+
 export const handleSectionNavigation = (id: string) => {
   const element = document.getElementById(id);
   const offset = 45;
@@ -11,3 +15,40 @@ export const handleSectionNavigation = (id: string) => {
     behavior: "smooth",
   });
 };
+
+const currentURL = window.location.href;
+const parts = currentURL.split('/');
+const baseUrl = parts[0] + '//' + parts[2];
+const staticPath = baseUrl + '/static/images/';
+
+export const globalVariables = {
+  currentURL,
+  baseUrl,
+  staticPath,
+};
+
+export const apiCall = async <T>(
+  method: string,
+  url: string,
+  data?: any
+): Promise<AxiosResponse<T>> => {
+  try {
+    const validMethods = ['get', 'post', 'put', 'delete'];
+
+    if (!validMethods.includes(method.toLowerCase())) {
+      throw new Error('Invalid HTTP method');
+    }
+
+    const response = await axios({
+      method: method.toLowerCase(),
+      url,
+      data,
+    });
+
+    return response;
+  } catch (error) {
+    console.error('API call error:', error);
+    throw error;
+  }
+};
+
