a0474e4b7ebf3d4939f13b9fde328fe308c87d6e
tinashe <tinashe@kartoza.com>
2023-11-22 08:18:51 +0200

    update files

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -14,10 +14,6 @@ echo 'Initialize project.'
 python manage.py collectstatic --clear --noinput
 python manage.py migrate
 
-# Load data from fixture files
-# echo 'Loading fixture data.'
-# python manage.py loaddata --ignore fixtures/*.json
-
 echo "-----------------------------------------------------"
 echo "FINISHED DJANGO ENTRYPOINT --------------------------"
 echo "-----------------------------------------------------"
