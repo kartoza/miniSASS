b31d620924b5b95c488e7a15aa41d6f81f840a76
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-22 02:00:22 +0200

    patch: ignore integrity errors

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -16,7 +16,7 @@ python manage.py migrate
 
 # Load data from fixture files
 echo 'Loading fixture data.'
-python manage.py loaddata fixtures/*.json
+python manage.py loaddata --ignore fixtures/*.json
 
 echo "-----------------------------------------------------"
 echo "FINISHED DJANGO ENTRYPOINT --------------------------"
