4ac7d5a6fb6f8ae6f09881a7c67de0c25383f221
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-22 15:45:40 +0200

    patch: clean up code

--- django_project/minisass_frontend/views.py
+++ django_project/minisass_frontend/views.py
@@ -1,19 +1,7 @@
 from django.http import HttpResponse
-from django.views.generic import TemplateView, View
-from monitor.models import Observations
-from minisass_authentication.models import UserProfile
-from monitor.models import Sites
-from decimal import Decimal
-import json  # Import the 'json' module instead of 'simplejson'
-from django.core.serializers.json import DjangoJSONEncoder
-from django.utils.translation import gettext as _
+from django.views.generic import TemplateView
 from django.conf import settings
 
-class DecimalEncoder(DjangoJSONEncoder):
-    def default(self, o):
-        if isinstance(o, Decimal):
-            return float(o)
-        return super(DecimalEncoder, self).default(o)
 
 class ReactHomeView(TemplateView):
     template_name = "react_base.html"
@@ -24,23 +12,3 @@ class ReactHomeView(TemplateView):
         )
         ctx['dev_mode'] = settings.DEBUG
         return ctx
-
-class ObservationsView(View):
-    def get(self, request, *args, **kwargs):
-        latest = Observations.objects.all().order_by('-time_stamp')[:20]
-
-        recent_observations = []
-
-        for observation in latest:
-            user_profile = UserProfile.objects.get(user=observation.user)
-
-            recent_observations.append({
-                'site': observation.site.site_name,
-                'username': user_profile.user.username,
-                'organisation': user_profile.organisation_name,
-                'time_stamp': observation.time_stamp,
-                'score': observation.score,
-            })
-
-        json_data = json.dumps(recent_observations, cls=DecimalEncoder)
-        return HttpResponse(json_data, content_type='application/json')
