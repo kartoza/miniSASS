b421c49725374d1f71d3a52ca348a9c64abbe19e
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-22 15:38:17 +0200

    feat: add observations urls

--- django_project/monitor/urls.py
+++ django_project/monitor/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
-from monitor.views import index, wms_get_feature_info, get_sites, get_closest_site, get_unique, zoom_observation, get_observations, download_observations, download_observations_filtered, get_schools, detail
+from monitor.views.observation import (
+    ObservationListCreateView, 
+    ObservationRetrieveUpdateDeleteView
+)
+from monitor.views import (
+    index, 
+    wms_get_feature_info, 
+    get_sites, 
+    get_closest_site, 
+    get_unique, 
+    zoom_observation, 
+    get_observations, 
+    download_observations, 
+    download_observations_filtered, 
+    get_schools, 
+    detail
+)
 
 # URL patterns for the miniSASS app
 urlpatterns = [
@@ -9,9 +25,25 @@ urlpatterns = [
     path('closest_site/<str:x>/<str:y>/<str:d>/', get_closest_site),
     path('unique/<str:field>/', get_unique),
     path('observation/<int:obs_id>/', zoom_observation),
-    path('observations/<int:site_id>/', get_observations),
+    path('observations-old/<int:site_id>/', get_observations),
     path('observations/download/<int:site_id>/', download_observations),
     path('observations/download/filtered/~<str:filter_string>', download_observations_filtered),
     path('schools/', get_schools),
     path('<int:monitor_id>/', detail, name='monitor_detail'),
+
+    path(
+        'observations/', 
+        ObservationListCreateView.as_view(), 
+        name='observation-list-create'
+    ),
+    path(
+        'observations/<int:pk>/', 
+        ObservationRetrieveUpdateDeleteView.as_view(), 
+        name='observation-retrieve-update-delete'
+    ),
+    path(
+        'observations/site/<int:site_id>/', 
+        ObservationListCreateView.as_view(), 
+        name='observation-list-by-site'
+    ),
 ]
