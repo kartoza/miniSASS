b4c076ac55da306d31ed41081da42a5d652172be
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-25 21:16:21 +0200

    patch: remove limit choices restriction

--- django_project/minisass_authentication/models.py
+++ django_project/minisass_authentication/models.py
@@ -18,36 +18,14 @@ class Lookup(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=False)
-    
-    SCHOOL = 'School'
-    NGO = 'NGO'
-    OTHER = 'Other'
-    UNIVERSITY = 'University'
-    CONSERVANCY = 'Conservancy'
-    PRIVATE_INDIVIDUAL = 'Private Individual'
-    CONSULTANCY = 'Consultancy'
-    GOVERNMENT_DEPARTMENT = 'Government Department'
-
-    ORGANISATION_TYPE_CHOICES = [
-        (SCHOOL, 'School'),
-        (NGO, 'NGO'),
-        (OTHER, 'Other'),
-        (UNIVERSITY, 'University'),
-        (CONSERVANCY, 'Conservancy'),
-        (PRIVATE_INDIVIDUAL, 'Private Individual'),
-        (CONSULTANCY, 'Consultancy'),
-        (GOVERNMENT_DEPARTMENT, 'Government Department'),
-    ]
-
-    organisation_type = models.CharField(
-        max_length=50,
-        choices=ORGANISATION_TYPE_CHOICES,
-        default=OTHER,
+    organisation_type = models.ForeignKey(
+        Lookup,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL
     )
-
     organisation_name = models.CharField(max_length=255, blank=True)
     country = models.CharField(max_length=255, blank=True, null=True)
-
 
     def __str__(self):
         return f"{self.organisation_type}: {self.organisation_name or 'Unknown'}"
