10476b6ca70a34b00c439788df2fcbca6fec184d
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-29 01:22:32 +0200

    Update entrypoint.sh

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -12,7 +12,7 @@ echo "-----------------------------------------------------"
 cd /home/web/django_project
 echo 'Initialize project.'
 python manage.py collectstatic --clear --noinput
-# python manage.py makemigrations
+python manage.py makemigrations
 python manage.py migrate
 
 # Run tests
