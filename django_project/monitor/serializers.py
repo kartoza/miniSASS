eb4bf6553273e91adac9a4009a36a0737e2e2c31
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-28 06:55:14 +0200

    patch: update serializer

--- django_project/monitor/serializers.py
+++ django_project/monitor/serializers.py
@@ -1,7 +1,45 @@
+from minisass_authentication.models import UserProfile
 from rest_framework import serializers
-from monitor.models import Observations
+from monitor.models import (
+    Observations,
+    Sites
+)
+class SitesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sites
+        fields = '__all__'
 
 class ObservationsSerializer(serializers.ModelSerializer):
+    site = SitesSerializer()
+    sitename = serializers.CharField(source='site.site_name')
+    rivername = serializers.CharField(source='site.river_name')
+    sitedescription = serializers.CharField(source='site.description')
+    rivercategory = serializers.CharField(source='site.river_cat')
+    longitude = serializers.FloatField(source='site.the_geom.x')
+    latitude = serializers.FloatField(source='site.the_geom.y')
+    collectorsname = serializers.SerializerMethodField()
+    organisationtype = serializers.SerializerMethodField()
+
     class Meta:
         model = Observations
         fields = '__all__'
+
+    def get_collectorsname(self, obj):
+        try:
+            user_profile = UserProfile.objects.get(user=obj.user)
+        except UserProfile.DoesNotExist:
+            user_profile = None
+
+        return (
+            f"{user_profile.user.first_name} {user_profile.user.last_name}"
+            if user_profile and user_profile.user.first_name and user_profile.user.last_name
+            else user_profile.user.username if user_profile else ""
+        )
+
+    def get_organisationtype(self, obj):
+        try:
+            user_profile = UserProfile.objects.get(user=obj.user)
+        except UserProfile.DoesNotExist:
+            user_profile = None
+
+        return user_profile.organisation_type if user_profile else ""
