144fc05e4b19b16ce1c74715dc75cd20f2475450
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-28 19:21:25 +0200

    Update index.tsx

--- django_project/minisass_frontend/src/components/ObservationDetails/index.tsx
+++ django_project/minisass_frontend/src/components/ObservationDetails/index.tsx
@@ -57,7 +57,7 @@ const ObservationDetails: React.FC<ObservationDetailsProps> = ({ setSidebarOpen 
 
   }, [observation_id]);
 
-  console.log(observationDetails)
+  console.log('results ',observationDetails)
 
   // this will be dynamic based on the observation data available TODO
   const tabsData = [
@@ -305,7 +305,7 @@ const ObservationDetails: React.FC<ObservationDetailsProps> = ({ setSidebarOpen 
                   className="text-gray-800_01 text-lg tracking-[0.15px] w-auto"
                   size="txtRalewayRomanRegular18"
                 >
-                  {observationDetails.organisationtype.description}
+                  {`observationDetails.organisationtype.description`}
                 </Text>
               </div>
             </div>
