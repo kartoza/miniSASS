e2859778140bbecfce92e409d67c1dbdc4a0f645
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-29 10:34:02 +0200

    Dynamic group scores (#298)
    * patch: add minisass to installed apss
    
    * feat: add group scores to admin
    
    * feat: define group scores model
    
    * feat: define serializer for group scores
    
    * feat: define group scores url
    
    * feat: define view for group scores
    
    * patch: show groups and scores defined in the db
    
    * Create __init__.py
    
    * patch: define migrations for group scores
    
    * patch: update variable names
    
    * Delete django_project/minisass/migrations/0001_intial.py
    
    * patch: merge migrations
    
    * feat: add tests for group scores

--- django_project/minisass/models.py
+++ django_project/minisass/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+
+class GroupScores(models.Model):
+    name = models.CharField(max_length=255)
+    sensitivity_score = models.DecimalField(max_digits=5, decimal_places=2)
+
 class Video(models.Model):
     title = models.CharField(max_length=255)
     embed_code = models.TextField()
