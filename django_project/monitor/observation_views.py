6bfc4e082708ae73487ffdf2741e9c0ff6a07a1c
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-11-30 04:42:02 +0200

    patch: make sure site is not a string

--- django_project/monitor/observation_views.py
+++ django_project/monitor/observation_views.py
@@ -51,7 +51,11 @@ def create_observations(request):
             diss_oxygen_unit = datainput.get('dissolvedoxygenOneUnit', 'mgl')
             elec_cond = Decimal(str(datainput.get('electricalconduOne', 0)))
             elec_cond_unit = datainput.get('electricalconduOneUnit', 'mS/m')
-            site_id = datainput.get('selectedSite',0)
+            site_id = datainput.get('selectedSite')
+            try:
+                site_id = int(site_id)
+            except (ValueError, TypeError):
+                site_id = 0
             longitude = Decimal(str(datainput.get('longitude')))
             latitude = Decimal(str(datainput.get('latitude')))
             obs_date = datainput.get('date')
