a6d441c889ba60c7950d78cf17eb440020ff7d8d
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-01 23:05:52 +0200

    patch: improve tests

--- django_project/minisass_authentication/test_views.py
+++ django_project/minisass_authentication/test_views.py
@@ -7,7 +7,7 @@ from django.utils.encoding import force_bytes
 from django.utils.http import (
     urlsafe_base64_encode
 )
-from minisass_authentication.custom_token_generator import custom_token_generator
+from django.contrib.auth.tokens import default_token_generator
 
 class ActivateAccountTestCase(TestCase):
     def setUp(self):
@@ -17,9 +17,8 @@ class ActivateAccountTestCase(TestCase):
             password='testpassword'
         )
         # Generate token with a specific duration
-        self.token_info = custom_token_generator.make_token(self.user, duration='days', count=6)
-        self.token = self.token_info['token']
-        self.expiration_time = self.token_info['expiration_time']
+        self.token_info = default_token_generator.make_token(self.user)
+        self.token = self.token_info
         self.uidb64 = urlsafe_base64_encode(force_bytes(self.user.pk))
         Lookup.objects.create(description='NGO')
         
