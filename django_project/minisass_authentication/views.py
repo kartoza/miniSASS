9728c14379ebefe2e7dcc87f3f60d0437c8555d4
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-01 22:54:17 +0200

    Update views.py

--- django_project/minisass_authentication/views.py
+++ django_project/minisass_authentication/views.py
@@ -18,7 +18,7 @@ from django.contrib.auth import (
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes,force_str, force_text
+from django.utils.encoding import force_bytes,force_str
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage, send_mail
 from django.conf import settings
@@ -148,7 +148,7 @@ def reset_password(request, uidb64, token):
 @api_view(['GET'])
 def activate_account(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
