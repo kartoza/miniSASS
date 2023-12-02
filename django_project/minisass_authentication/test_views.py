a70147379ca8c93f62d8dac7cc51389dd22d6a80
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-02 02:03:41 +0200

    feat: add request password reset tests

--- django_project/minisass_authentication/test_views.py
+++ django_project/minisass_authentication/test_views.py
@@ -9,6 +9,53 @@ from django.utils.http import (
 )
 from minisass_authentication.email_verification_token import email_verification_token
 
+
+class PasswordResetTest(TestCase):
+    def setUp(self):
+        self.user_data = {
+            'email': 'test@example.com'
+        }
+        self.user = User.objects.create_user(**self.user_data)
+
+    def test_request_password_reset(self):
+        url = reverse('request_password_reset')
+        data = {'email': self.user_data['email']}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200) 
+
+        # Check if the token and UID are saved in the user model
+        user = User.objects.get(email=self.user_data['email'])
+        self.assertIsNotNone(user.password_reset_token)
+        self.assertIsNotNone(user.password_reset_uid)
+
+    def test_verify_password_reset(self):
+        # Generate token and UID for user
+        token = default_token_generator.make_token(self.user)
+        uidb64 = urlsafe_base64_encode(force_bytes(self.user.pk))
+
+        # Verify password reset link
+        url = reverse('verify_password_reset', kwargs={'uidb64': uidb64, 'token': token})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200) 
+
+
+
+    def test_update_password_reset(self):
+        # Generate token and UID for user
+        token = default_token_generator.make_token(self.user)
+        uidb64 = urlsafe_base64_encode(force_bytes(self.user.pk))
+
+        # Update password using reset link
+        new_password = 'newpassword123'
+        url = reverse('update_password_reset', kwargs={'uidb64': uidb64, 'token': token})
+        data = {'new_password': new_password}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200) 
+
+        # Validate if the password is updated
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password(new_password))
+        
 class ActivateAccountTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
