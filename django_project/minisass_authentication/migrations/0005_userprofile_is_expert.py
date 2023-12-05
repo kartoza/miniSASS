59f49d845516f60a123891e783f2774680735cee
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-05 23:43:38 +0200

    patch: update mmiration

--- django_project/minisass_authentication/migrations/0005_userprofile_is_expert.py
+++ django_project/minisass_authentication/migrations/0005_userprofile_is_expert.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('minisass_authentication'),
+        ('minisass_authentication','0004_alter_lookup_id_alter_userprofile_id'),
     ]
 
     operations = [
