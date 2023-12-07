e96f16a7b2a2aa7e7dd0086cd99183962cde75da
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-07 23:42:50 +0200

    feat: define site observations serializer #369

--- django_project/monitor/serializers.py
+++ django_project/monitor/serializers.py
@@ -50,3 +50,50 @@ class ObservationsSerializer(serializers.ModelSerializer):
             return serialized_organisation_type
         else:
             return None
+
+class SitesWithObservationsSerializer(serializers.ModelSerializer):
+    observations = ObservationsSerializer(many=True, read_only=True)
+    sitename = serializers.CharField(source='site.site_name')
+    rivername = serializers.CharField(source='site.river_name')
+    sitedescription = serializers.CharField(source='site.description')
+    rivercategory = serializers.CharField(source='site.river_cat')
+    longitude = serializers.FloatField(source='site.the_geom.x')
+    latitude = serializers.FloatField(source='site.the_geom.y')
+
+
+    class Meta:
+        model = Sites
+        fields = '__all__'
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        # Combine the site information to the observations
+        combined_data = {
+            'site': {
+                'sitename': data['sitename'],
+                'rivername': data['rivername'],
+                'rivercategory': data['rivercategory'],
+                'sitedescription': data['sitedescription'],
+            },
+            'observations': [],
+        }
+
+        for observation_data in data['observations']:
+            user_profile = UserProfile.objects.filter(user=observation_data['user']).first()
+            collectors_name = (
+                f"{user_profile.user.first_name} {user_profile.user.last_name}"
+                if user_profile and user_profile.user.first_name and user_profile.user.last_name
+                else user_profile.user.username if user_profile else ""
+            )
+
+            observation_info = {
+                'collectorsname': collectors_name,
+                'organisationtype': LookupSerializer(user_profile.organisation_type).data if user_profile else None,
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'obs_date': observation_data['obs_date'],
+                'observationImages': [],
+            }
+            combined_data['observations'].append(observation_info)
+
+        return combined_data
