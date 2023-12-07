ff9199f402b2b06e13522683193bcdd388ecf71a
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-07 22:24:43 +0200

    patch: update url #369

--- django_project/minisass_frontend/src/components/Map/Map.tsx
+++ django_project/minisass_frontend/src/components/Map/Map.tsx
@@ -348,7 +348,7 @@ export const Map = forwardRef((props: Interface, ref) => {
     const captureCoordinatesAndQuery = async (latitude, longitude) => {
       try {
 
-        const response = await axios.get(`${globalVariables.baseUrl}/monitor/sites?latitude=${latitude}&longitude=${longitude}`);
+        const response = await axios.get(`${globalVariables.baseUrl}/monitor/site-observations?latitude=${latitude}&longitude=${longitude}`);
     
         if (response.data.length > 0) {
           props.openObservationForm(response.data);
