d9fed0fa0f8f8371f308646bf44c925d2e16e869
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-07 23:44:00 +0200

    feat: define view for fetching site observations

--- django_project/monitor/site_views.py
+++ django_project/monitor/site_views.py
@@ -2,7 +2,11 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from monitor.models import Sites
-from monitor.serializers import SitesSerializer
+from monitor.serializers import (
+    SitesSerializer, 
+    SitesWithObservationsSerializer
+)
+from rest_framework.views import APIView
 
 class SitesListCreateView(generics.ListCreateAPIView):
     queryset = Sites.objects.all()
@@ -11,3 +15,12 @@ class SitesListCreateView(generics.ListCreateAPIView):
 class SiteRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Sites.objects.all()
     serializer_class = SitesSerializer
+
+class SiteObservationsByLocation(APIView):
+    def get(self, request, latitude, longitude):
+        try:
+            site = Sites.objects.get(the_geom__equals=(latitude, longitude))
+            serializer = SitesWithObservationsSerializer(site)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Sites.DoesNotExist:
+            return Response([], status=status.HTTP_404_NOT_FOUND)
