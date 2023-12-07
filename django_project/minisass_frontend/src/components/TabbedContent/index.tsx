9748f7ad6c04da310029b2767c60b8fdbf12bdf6
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-07 22:04:42 +0200

    feat: updated tabbed content component #369

--- django_project/minisass_frontend/src/components/TabbedContent/index.tsx
+++ django_project/minisass_frontend/src/components/TabbedContent/index.tsx
@@ -1,28 +1,26 @@
 import React, { useState } from 'react';
 
-const TabbedContent = ({ tabsData }) => {
-  const [selectedTab, setSelectedTab] = useState(tabsData[0].id);
-
-  const handleTabChange = (tabId) => {
-    setSelectedTab(tabId);
+const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
+  const handleTabChange = (tabIndex) => {
+    onTabChange(tabIndex);
   };
 
   return (
     <div className="flex flex-col items-start justify-start w-full">
       <div className="flex gap-5">
-        {tabsData.map((tab) => (
+        {tabsData.map((tab, index) => (
           <button
             key={tab.id}
-            className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${selectedTab === tab.id ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
-            onClick={() => handleTabChange(tab.id)}
+            className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${activeTabIndex === index ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
+            onClick={() => handleTabChange(index)}
           >
             {tab.label}
           </button>
         ))}
       </div>
       <div className="w-full">
-        {tabsData.map((tab) => (
-          <div key={`content_${tab.id}`} style={{ display: selectedTab === tab.id ? 'block' : 'none' }}>
+        {tabsData.map((tab, index) => (
+          <div key={`content_${tab.id}`} style={{ display: activeTabIndex === index ? 'block' : 'none' }}>
             {tab.content}
           </div>
         ))}
