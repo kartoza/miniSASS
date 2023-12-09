238381684b90cc11a7ac9a2f18d6094b9c81111b
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-09 07:43:42 +0200

    patch: make page form scroll bar accessible

--- django_project/minisass_frontend/src/components/Sidebar/index.tsx
+++ django_project/minisass_frontend/src/components/Sidebar/index.tsx
@@ -28,7 +28,7 @@ const Sidebar: React.FC<SidebarProps> = ({
   return (
     <div
       className={`absolute ${
-        isOpen ? "right-0" : "-right-full"
+        isOpen ? "right-[10px]" : "-right-full"
       } bg-white-A700 flex flex-col  items-start justify-center py-5 rounded-bl-[10px] rounded-br-[10px] rounded-tr-[10px] top-[0%] sm:top-[25px] w-auto transition-all duration-300`}
     >
       {isObservationDetails ? (
