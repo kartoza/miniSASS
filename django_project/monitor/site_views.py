b67606f3deb607865552bfad14f865df585aeb30
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-09 09:10:27 +0200

    feat: define assessment views

--- django_project/monitor/site_views.py
+++ django_project/monitor/site_views.py
@@ -2,7 +2,10 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from monitor.models import Sites
-from monitor.serializers import SitesSerializer
+from monitor.serializers import (
+    SitesSerializer,
+    AssessmentSerializer
+)
 
 class SitesListCreateView(generics.ListCreateAPIView):
     queryset = Sites.objects.all()
@@ -11,3 +14,11 @@ class SitesListCreateView(generics.ListCreateAPIView):
 class SiteRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Sites.objects.all()
     serializer_class = SitesSerializer
+
+class AssessmentListCreateView(generics.ListCreateAPIView):
+    queryset = Assessment.objects.all()
+    serializer_class = AssessmentSerializer
+
+class AssessmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Assessment.objects.all()
+    serializer_class = AssessmentSerializer
