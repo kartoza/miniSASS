3e60ece321d0b756994bfd4ce8faf1d2963f8580
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-10 20:23:50 +0200

    patch: add reset map prop

--- django_project/minisass_frontend/src/components/Map/Map.tsx
+++ django_project/minisass_frontend/src/components/Map/Map.tsx
@@ -20,6 +20,7 @@ interface Interface {
   overlayLayers: layerConfiguration[],
   handleSelect: (latitude: number, longitude: number) => void;
   selectingOnMap: boolean;
+  resetMap: boolean;
   selectedCoordinates: {latitude: number, longitude: number};
   idxActive: number;
   setIdxActive: React.Dispatch<React.SetStateAction<number>>;
@@ -334,6 +335,21 @@ export const Map = forwardRef((props: Interface, ref) => {
         removeClickEventListener();
       };
     }, [props.handleSelect, props.selectingOnMap,props.selectedCoordinates]);
+
+    useEffect(() => {
+      if (props.resetMap === true) {
+        let mapInstance = map;
+  
+        if (mapInstance) {
+          mapInstance.flyTo({
+            center: [initialMapConfig.center[0], initialMapConfig.center[1]],
+            zoom: initialMapConfig.zoom,
+            essential: true,
+          });
+        }
+        props.resetCoordinates()
+      }
+    }, [props.resetMap]);
 
     return <div id="map" className="w-full h-full bg-slate-200">
       {
