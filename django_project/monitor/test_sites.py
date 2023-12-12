1ac25a78134db463934c4ab67b65a9df8adb37ca
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-12 12:26:10 +0200

    feat: add tests for create site with images

--- django_project/monitor/test_sites.py
+++ django_project/monitor/test_sites.py
@@ -6,6 +6,90 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from monitor.models import Sites, Observations
 from monitor.serializers import SitesWithObservationsSerializer
+from rest_framework.test import APIClient
+from io import BytesIO
+from PIL import Image
+
+class SitesListCreateViewTestCase(TestCase):
+    def setUp(self):
+        # Create a user for authentication
+        self.user = User.objects.create_user(username='testuser', password='testpassword', email='test@example.com')
+
+    def test_create_site_with_images(self):
+        client = APIClient()
+        client.force_authenticate(user=self.user)
+
+        # Create a mock image
+        image = Image.new('RGB', (100, 100))
+        image_file = BytesIO()
+        image.save(image_file, 'PNG')
+        image_file.name = 'test_image.png'
+
+        # Prepare the request data
+        data = {
+            'site_data': {
+                'site_name': 'Test Site',
+                'river_name': 'Test River',
+                'description': 'Test Description',
+                'river_cat': 'rocky',
+                'longitude': 0,
+                'latitude': 0,
+            },
+            'images': [image_file],
+        }
+
+        # Make a POST request to create a site with images
+        url = reverse('sites-list-create')
+        response = client.post(url, data, format='multipart')
+
+        # Check if the response status code is 201 Created
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check if the site and images are created
+        self.assertEqual(response.data['site_name'], 'Test Site')
+        self.assertEqual(response.data['river_name'], 'Test River')
+        self.assertEqual(response.data['description'], 'Test Description')
+
+        # Ensure the images are attached to the site
+        site_id = response.data['gid']
+        self.assertEqual(site_id, 1)  # Update with the actual site ID
+        images_count = SiteImage.objects.filter(site_id=site_id).count()
+        self.assertEqual(images_count, 1)
+
+    def test_create_site_without_images(self):
+        client = APIClient()
+        client.force_authenticate(user=self.user)
+
+        # Prepare the request data without images
+        data = {
+            'site_data': {
+                'site_name': 'Test Site',
+                'river_name': 'Test River',
+                'description': 'Test Description',
+                'river_cat': 'rocky',
+                'longitude': 0,
+                'latitude': 0,
+            },
+            'images': [],
+        }
+
+        # Make a POST request to create a site without images
+        url = reverse('sites-list-create')
+        response = client.post(url, data)
+
+        # Check if the response status code is 201 Created
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check if the site is created without images
+        self.assertEqual(response.data['site_name'], 'Test Site')
+        self.assertEqual(response.data['river_name'], 'Test River')
+        self.assertEqual(response.data['description'], 'Test Description')
+
+        # Ensure no images are attached to the site
+        site_id = response.data['gid']
+        images_count = SiteImage.objects.filter(site_id=site_id).count()
+        self.assertEqual(images_count, 0)
+
 
 class SiteObservationsByLocationTestCase(APITestCase):
     def setUp(self):
