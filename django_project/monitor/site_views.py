cecb7825a5d49554a475554194886033b0190c9f
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2023-12-12 11:52:13 +0200

    feat: ovveride default site create view

--- django_project/monitor/site_views.py
+++ django_project/monitor/site_views.py
@@ -3,8 +3,9 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from monitor.models import SiteImage, Sites, Assessment
 
-from monitor.models import Sites, Assessment
+
 from monitor.serializers import (
     AssessmentSerializer,
     SitesSerializer,
@@ -15,6 +16,43 @@ from monitor.serializers import (
 class SitesListCreateView(generics.ListCreateAPIView):
     queryset = Sites.objects.all()
     serializer_class = SitesSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Extract data from the request payload
+        site_data = request.data.get('site_data', {})
+        images = request.FILES.getlist('images', [])
+
+        # Extract site data
+        site_name = site_data.get('site_name', '')
+        river_name = site_data.get('river_name', '')
+        description = site_data.get('description', '')
+        river_cat = site_data.get('river_cat', '')
+        longitude = site_data.get('longitude', 0)
+        latitude = site_data.get('latitude', 0)
+
+        # Get the user from the request object
+        user = request.user
+
+        # Create a new site
+        site = Sites.objects.create(
+            site_name=site_name,
+            river_name=river_name,
+            description=description,
+            river_cat=river_cat,
+            the_geom=Point(x=longitude, y=latitude, srid=4326),
+            user=user
+        )
+
+        # Save images for the site
+        for image in images:
+            SiteImage.objects.create(
+                site=site, image=image
+            )
+
+        # Serialize the created site and return the response
+        serializer = self.get_serializer(site)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 class SiteRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Sites.objects.all()
