7cf2a8f08a4e4b8ed1e30c2d46bea2c8a5e058e5
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-01-03 00:45:07 +0200

    patch: add a horizontal scroll to the observation tabs

--- django_project/minisass_frontend/src/components/TabbedContent/index.tsx
+++ django_project/minisass_frontend/src/components/TabbedContent/index.tsx
@@ -6,24 +6,26 @@ const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
   };
 
   return (
-    <div className="flex flex-col items-start justify-start w-full">
-      <div className="flex gap-5">
-        {tabsData.map((tab, index) => (
-          <button
-            key={tab.id}
-            className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${activeTabIndex === index ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
-            onClick={() => handleTabChange(index)}
-          >
-            {tab.label}
-          </button>
-        ))}
-      </div>
-      <div className="w-full">
-        {tabsData.map((tab, index) => (
-          <div key={`content_${tab.id}`} style={{ display: activeTabIndex === index ? 'block' : 'none' }}>
-            {tab.content}
-          </div>
-        ))}
+    <div className="w-full overflow-x-auto">
+      <div className="flex flex-col items-start justify-start">
+        <div className="flex gap-5">
+          {tabsData.map((tab, index) => (
+            <button
+              key={tab.id}
+              className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${activeTabIndex === index ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
+              onClick={() => handleTabChange(index)}
+            >
+              {tab.label}
+            </button>
+          ))}
+        </div>
+        <div className="w-full">
+          {tabsData.map((tab, index) => (
+            <div key={`content_${tab.id}`} style={{ display: activeTabIndex === index ? 'block' : 'none' }}>
+              {tab.content}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
