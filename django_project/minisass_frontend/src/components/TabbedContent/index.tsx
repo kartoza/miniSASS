3f9193fa49c35b49b93c8fd878fc395f60aa4292
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-01-07 00:33:13 +0200

    patch: fix horizontal scroll not appearing

--- django_project/minisass_frontend/src/components/TabbedContent/index.tsx
+++ django_project/minisass_frontend/src/components/TabbedContent/index.tsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
 
-const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
-  const handleTabChange = (tabIndex) => {
-    onTabChange(tabIndex);
+const TabbedContent = ({ tabsData }) => {
+  const [selectedTab, setSelectedTab] = useState(tabsData[0].id);
+
+  const handleTabChange = (tabId) => {
+    setSelectedTab(tabId);
   };
 
   return (
-    <div className="w-full overflow-x-auto">
-      <div className="flex flex-col items-start justify-start">
-        <div className="flex gap-5">
-          {tabsData.map((tab, index) => (
-            <button
-              key={tab.id}
-              className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${activeTabIndex === index ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
-              onClick={() => handleTabChange(index)}
-            >
-              {tab.label}
-            </button>
-          ))}
-        </div>
-        <div className="w-full">
-          {tabsData.map((tab, index) => (
-            <div key={`content_${tab.id}`} style={{ display: activeTabIndex === index ? 'block' : 'none' }}>
-              {tab.content}
-            </div>
-          ))}
-        </div>
+    <div className="flex flex-col items-start justify-start w-[28vw]" style={{overflowX: 'auto', overflowY: 'hidden'}}>
+      <div className="flex gap-5">
+        {tabsData.map((tab) => (
+          <button
+            key={tab.id}
+            className={`cursor-pointer min-w-[126px] text-base text-center ${selectedTab === tab.id && 'active'}`}
+            onClick={() => handleTabChange(tab.id)}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className="w-full">
+        {tabsData.map((tab) => (
+          <div key={`content_${tab.id}`} style={{ display: selectedTab === tab.id ? 'block' : 'none' }}>
+            {tab.content}
+          </div>
+        ))}
       </div>
     </div>
   );
