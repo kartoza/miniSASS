f9f145b2907b5acc7b141cfd7c67030c1f747a30
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-01-09 15:06:14 +0200

    feat: update file upload implementation

--- django_project/monitor/observation_views.py
+++ django_project/monitor/observation_views.py
@@ -26,6 +26,8 @@ from monitor.serializers import (
     ObservationPestImageSerializer, 
     ObservationsAllFieldsSerializer
 )
+from django.core.exceptions import ValidationError
+from django.db import transaction
 
 def get_observations_by_site(request, site_id, format=None):
     try:
@@ -38,6 +40,119 @@ def get_observations_by_site(request, site_id, format=None):
         )
     except Sites.DoesNotExist:
         raise Http404("Site does not exist")
+
+
+
+@csrf_exempt
+@login_required
+def upload_pest_image(request):
+    """
+    This view function handles the upload of pest images, associating them with an observation and a site.
+    
+    - Creates an empty site and observation.
+    - Associates uploaded images with the observation.
+    - Returns the observation ID, site ID, and image IDs for further processing.
+
+    Note:
+    - The function saves user uploads before saving the observation to facilitate AI calculations on the images.
+    - The returned image IDs can be used for image deletion if the user decides to change their selection.
+
+    Expected Data:
+    - 'datainput' object: Contains additional information about the observation.
+    - 'observationID': Specifies the ID for the observation.
+    - Uploaded files: Images to be associated with the observation.
+
+    :param request: The HTTP request object.
+    :return: JsonResponse with status, observation ID, site ID, and pest image IDs.
+    """
+    if request.method == 'POST':
+        try:
+            with transaction.atomic():
+                data = json.loads(request.POST.get('data', '{}'))
+                datainput = data.get('datainput', {})
+
+                site_id = datainput.get('selectedSite')
+                try:
+                    site_id = int(site_id)
+                except (ValueError, TypeError):
+                    site_id = 0
+
+                try:
+                    site = Sites.objects.get(gid=site_id)
+                except Sites.DoesNotExist:
+                    max_site_id = Sites.objects.all().aggregate(Max('gid'))['gid__max']
+                    new_site_id = max_site_id + 1 if max_site_id is not None else 1
+
+                user = request.user
+
+                site = Sites.objects.create(
+                    gid=new_site_id,
+                    the_geom=Point(x=0, y=0, srid=4326),
+                    user=user
+                )
+
+
+                observation_id = request.POST.get('observationId')
+                try:
+                    observation = Observations.objects.get(gid=observation_id, site=site)
+                except Observations.DoesNotExist:
+                    max_observation_id = Observations.objects.all().aggregate(Max('gid'))['gid__max']
+                    new_observation_id = max_observation_id + 1 if max_observation_id is not None else 1
+
+                    observation = Observations.objects.create(
+                        gid=new_observation_id,
+                        site=site
+                    )
+
+                # Save images in the request object
+                for key, image in request.FILES.items():
+                    if 'pest_' in key:
+                        pest_name = key.split(':')[1]
+                        if pest_name:
+                            pest, _ = Pest.objects.get_or_create(
+                                name=pest_name.replace('_', ' ').capitalize()
+                            )
+                            pest_image, _ = ObservationPestImage.objects.get_or_create(
+                                observation=observation,
+                                pest=pest
+                            )
+                            pest_image.image = image
+                            pest_image.save()
+
+                return JsonResponse(
+                    {
+                        'status': 'success', 
+                        'observation_id': observation.gid,
+                        'site_id': site.gid,
+                        'pest_image_id': pest_image.id
+                    }
+                )
+        except ValidationError as ve:
+            # Handle validation errors (e.g., invalid data)
+            return JsonResponse({'status': 'error', 'message': str(ve)})
+        except Exception as e:
+            # Handle other exceptions
+            return JsonResponse({'status': 'error', 'message': str(e)})
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+
+@csrf_exempt
+@login_required
+def delete_pest_image(request, *args, **kwargs):
+    if request.method == 'POST':
+        try:
+            observation_id = kwargs.get('observation_pk')
+            image_id = kwargs.get('pk')
+
+            observation = get_object_or_404(Observations, gid=observation_id)
+
+            image = get_object_or_404(ObservationPestImage, id=image_id, observation=observation)
+
+            image.delete()
+
+            return JsonResponse({'status': 'success', 'message': 'Image deleted successfully.'})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
 
 
 @csrf_exempt
@@ -73,24 +188,14 @@ def create_observations(request):
             diss_oxygen_unit = datainput.get('dissolvedoxygenOneUnit', 'mgl')
             elec_cond = Decimal(str(datainput.get('electricalconduOne', 0)))
             elec_cond_unit = datainput.get('electricalconduOneUnit', 'mS/m')
-            site_id = datainput.get('selectedSite')
-            try:
-                site_id = int(site_id)
-            except (ValueError, TypeError):
-                site_id = 0
+            site_id = request.POST.get('siteId')
+            observation_id = request.POST.get('observationId')
             obs_date = datainput.get('date')
-
-            # Get the user from the request object
             user = request.user
 
-            # Check if the site with the given site_id exists
             try:
                 site = Sites.objects.get(gid=site_id)
-            except Sites.DoesNotExist:
-                # If it doesn't exist, create a new site with an incremented ID
-                max_site_id = Sites.objects.all().aggregate(Max('gid'))['gid__max']
-                new_site_id = max_site_id + 1 if max_site_id is not None else 1
-
+                
                 site_name = datainput.get('siteName', '')
                 river_name = datainput.get('riverName', '')
                 description = datainput.get('siteDescription', '')
@@ -98,68 +203,59 @@ def create_observations(request):
                 longitude = datainput.get('longitude', 0)
                 latitude = datainput.get('latitude', 0)
 
-                # Save the new site with the incremented ID
-                site = Sites.objects.create(
-                    gid=new_site_id,
-                    site_name=site_name,
-                    river_name=river_name,
-                    description=description,
-                    river_cat=river_cat,
-                    the_geom=Point(x=longitude, y=latitude, srid=4326),
-                    user=user
-                )
+                site.site_name = site_name
+                site.river_name = river_name
+                site.description = description
+                site.river_cat = river_cat
+                site.the_geom = Point(x=longitude, y=latitude, srid=4326)
+                site.user = user
+                site.save()
 
-            # Save images
-            for key, image in request.FILES.items():
-                if 'image_' in key:
-                    SiteImage.objects.create(
-                        site=site, image=image
-                    )
-
-            max_observation_id = Observations.objects.all().aggregate(Max('gid'))['gid__max']
-            new_observation_id = max_observation_id + 1 if max_observation_id is not None else 1
-            observation = Observations.objects.create(
-                gid=new_observation_id,
-                score=score,
-                site=site,
-                user=user,
-                flatworms=flatworms,
-                leeches=leeches,
-                crabs_shrimps=crabs_shrimps,
-                stoneflies=stoneflies,
-                minnow_mayflies=minnow_mayflies,
-                other_mayflies=other_mayflies,
-                damselflies=damselflies,
-                dragonflies=dragonflies,
-                bugs_beetles=bugs_beetles,
-                caddisflies=caddisflies,
-                true_flies=true_flies,
-                snails=snails,
-                comment=comment,
-                water_clarity=water_clarity,
-                water_temp=water_temp,
-                ph=ph,
-                diss_oxygen=diss_oxygen,
-                diss_oxygen_unit=diss_oxygen_unit,
-                elec_cond=elec_cond,
-                elec_cond_unit=elec_cond_unit,
-                obs_date=obs_date
-            )
-
-            # Save images
-            for key, image in request.FILES.items():
-                if 'pest_' in key:
-                    pest = key.split(':')[1]
-                    if pest:
-                        pest, _ = Pest.objects.get_or_create(
-                            name=pest.replace('_', ' ').capitalize()
+                for key, image in request.FILES.items():
+                    if 'image_' in key:
+                        SiteImage.objects.create(
+                            site=site, image=image
                         )
-                        pest_image, _ = ObservationPestImage.objects.get_or_create(
-                            observation=observation,
-                            pest=pest
-                        )
-                        pest_image.image = image
-                        pest_image.save()
+
+            except Sites.DoesNotExist:
+                pass
+
+
+            try:
+                observation = Observations.objects.get(gid=observation_id)
+
+                observation.score = score
+                observation.site = site
+                observation.user = user
+                observation.flatworms = flatworms
+                observation.leeches = leeches
+                observation.crabs_shrimps = crabs_shrimps
+                observation.stoneflies = stoneflies
+                observation.minnow_mayflies = minnow_mayflies
+                observation.other_mayflies = other_mayflies
+                observation.damselflies = damselflies
+                observation.dragonflies = dragonflies
+                observation.bugs_beetles = bugs_beetles
+                observation.caddisflies = caddisflies
+                observation.true_flies = true_flies
+                observation.snails = snails
+                observation.comment = comment
+                observation.water_clarity = water_clarity
+                observation.water_temp = water_temp
+                observation.ph = ph
+                observation.diss_oxygen = diss_oxygen
+                observation.diss_oxygen_unit = diss_oxygen_unit
+                observation.elec_cond = elec_cond
+                observation.elec_cond_unit = elec_cond_unit
+                observation.obs_date = obs_date
+
+                observation.save()
+
+            except Observations.DoesNotExist:
+                pass
+
+
+
 
             return JsonResponse(
                 {'status': 'success', 'observation_id': observation.gid})
