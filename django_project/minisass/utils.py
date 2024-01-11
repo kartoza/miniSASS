2dc41780e1a10303479cd0af106bdbf643821412
Zakki <age.zakki@gmail.com>
2024-01-11 16:30:36 +0700

    Align how to title

--- django_project/minisass/utils.py
+++ django_project/minisass/utils.py
@@ -1,4 +1,6 @@
 import os
+import boto3
+from django.conf import settings
 
 # Absolute filesystem path to the Django project directory:
 DJANGO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -12,5 +14,22 @@ def absolute_path(*args):
 def delete_file_field(file_field):
     """Delete actual file from file_field."""
     if file_field:
-        if os.path.isfile(file_field.path):
-            os.remove(file_field.path)
+        try:
+            if os.path.isfile(file_field.path):
+                os.remove(file_field.path)
+        except Exception:
+            pass
+
+        try:
+            s3 = boto3.client(
+                's3',
+                endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
+            )
+            s3.delete_object(
+                Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+                Key=file_field.name,
+            )
+        except Exception:
+            pass
