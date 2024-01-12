ced811681b998e0e375f7f86acccaf2ad00deefa
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-01-12 03:56:13 +0200

    patch: prevent observation panel from closing

--- django_project/minisass_frontend/src/components/TabbedContent/index.tsx
+++ django_project/minisass_frontend/src/components/TabbedContent/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
+  // Sort tabsData by the date in descending order
+  const sortedTabsData = [...tabsData].sort((a, b) => new Date(b.label) - new Date(a.label));
+
   const handleTabChange = (tabIndex) => {
     onTabChange(tabIndex);
   };
@@ -8,7 +11,7 @@ const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
   return (
     <div className="flex flex-col items-start justify-start w-full">
       <div className="flex gap-5">
-        {tabsData.map((tab, index) => (
+        {sortedTabsData.map((tab, index) => (
           <button
             key={tab.id}
             className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${activeTabIndex === index ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
@@ -19,7 +22,7 @@ const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
         ))}
       </div>
       <div className="w-full">
-        {tabsData.map((tab, index) => (
+        {sortedTabsData.map((tab, index) => (
           <div key={`content_${tab.id}`} style={{ display: activeTabIndex === index ? 'block' : 'none' }}>
             {tab.content}
           </div>
