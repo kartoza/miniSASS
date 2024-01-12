046ad3783d85e3a7c83b38a4f6aa226aeb66e4a1
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-01-12 04:44:01 +0200

    patch: add horizontal scroll to multiple tabs

--- django_project/minisass_frontend/src/components/TabbedContent/index.tsx
+++ django_project/minisass_frontend/src/components/TabbedContent/index.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 
 const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
+  // Sort tabsData by the date in descending order
+  const sortedTabsData = [...tabsData].sort((a, b) => new Date(b.label) - new Date(a.label));
+
   const handleTabChange = (tabIndex) => {
     onTabChange(tabIndex);
   };
 
   return (
-    <div className="flex flex-col items-start justify-start w-full">
-      <div className="flex gap-5">
-        {tabsData.map((tab, index) => (
+    <div className="flex flex-col items-start justify-start " >
+      <div className="flex gap-5 w-[28vw]" style={{overflowX: 'auto'}}>
+        {sortedTabsData.map((tab, index) => (
           <button
             key={tab.id}
             className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${activeTabIndex === index ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
@@ -18,8 +21,8 @@ const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
           </button>
         ))}
       </div>
-      <div className="w-full">
-        {tabsData.map((tab, index) => (
+      <div className="w-full" style={{marginTop: '-10px'}}>
+        {sortedTabsData.map((tab, index) => (
           <div key={`content_${tab.id}`} style={{ display: activeTabIndex === index ? 'block' : 'none' }}>
             {tab.content}
           </div>
