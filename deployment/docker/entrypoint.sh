8ee363c4f6367e93fa643cd47497b9f4a16d1419
Admire Nyakudya <addloe@gmail.com>
2024-01-17 15:59:10 +0200

    add SQL for intersects

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -25,6 +25,14 @@ export PGPASSWORD="${POSTGRES_PASS}"
 
 psql -d "${DJANGO_DB}" -p 5432 -U "${POSTGRES_USER}" -h "${DATABASE_HOST}" -c 'DROP VIEW IF EXISTS public.minisass_observations;'
 
+RESULT=$(psql "${DJANGO_DB}" -U "${POSTGRES_USER}" -p 5432 -h "${DATABASE_HOST}" -c "SELECT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = 'admin_countries')")
+if [ "${RESULT}" != "t" ]; then
+  psql -d "${DJANGO_DB}" -p 5432 -U "${POSTGRES_USER}" -h "${DATABASE_HOST}" -f /home/web/django_project/webmapping/sql/admin_countries.sql
+  psql -d "${DJANGO_DB}" -p 5432 -U "${POSTGRES_USER}" -h "${DATABASE_HOST}" -f /home/web/django_project/webmapping/sql/intersect.sql
+
+fi
+
+
 pushd /home/web/django_project || exit
 echo 'Initialize project.'
 python manage.py collectstatic --clear --noinput
@@ -50,52 +58,7 @@ python manage.py shell -c "from django.contrib.auth.models import User; User.obj
 echo 'Running tests.'
 python manage.py test
 
-psql -d "${DJANGO_DB}" -p 5432 -U "${POSTGRES_USER}" -h "${DATABASE_HOST}" -c 'CREATE OR REPLACE VIEW public.minisass_observations
- AS
- SELECT sites.gid AS sites_gid,
-    sites.river_name,
-    sites.site_name,
-    sites.description as site_description,
-    sites.river_cat,
-    observations.gid AS observations_gid,
-    observations.score,
-    observations.water_clarity,
-    observations.water_temp,
-    observations.ph,
-    observations.diss_oxygen,
-    observations.diss_oxygen_unit,
-    observations.elec_cond,
-    observations.elec_cond_unit,
-    observations.obs_date,
-    observations.comment,
-    observations.flatworms,
-    observations.worms,
-    observations.leeches,
-    observations.crabs_shrimps,
-    observations.stoneflies,
-    observations.minnow_mayflies,
-    observations.other_mayflies,
-    observations.damselflies,
-    observations.dragonflies,
-    observations.bugs_beetles,
-    observations.caddisflies,
-    observations.true_flies,
-    observations.snails,
-    observations.flag,
-    auth_user.username,
-    minisass_authentication_userprofile.organisation_name,
-    minisass_authentication_lookup.description AS organisation_type,
-	sites.the_geom,
-    st_x(sites.the_geom) AS x,
-    st_y(sites.the_geom) AS y
-   FROM sites
-     LEFT JOIN observations ON sites.gid = observations.site_id
-     LEFT JOIN auth_user ON observations.user_id = auth_user.id
-     LEFT JOIN minisass_authentication_userprofile ON auth_user.id = minisass_authentication_userprofile.user_id
-     LEFT JOIN minisass_authentication_lookup ON minisass_authentication_userprofile.organisation_type_id = minisass_authentication_lookup.id
-	 LEFT JOIN monitor_siteimage ON sites.gid = monitor_siteimage.site_id
-	 LEFT JOIN monitor_observationpestimage ON observations.gid = monitor_observationpestimage.observation_id
-  ORDER BY observations.obs_date;'
+psql -d "${DJANGO_DB}" -p 5432 -U "${POSTGRES_USER}" -h "${DATABASE_HOST}" -f /home/web/django_project/webmapping/sql/observation.sql
 
 echo "-----------------------------------------------------"
 echo "FINISHED DJANGO ENTRYPOINT --------------------------"
