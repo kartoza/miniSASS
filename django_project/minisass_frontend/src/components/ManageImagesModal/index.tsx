a5b94faba9e8441c053148a6cb1a235b2f8005ec
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-01-22 09:05:30 +0200

    patch: remove break statement

--- django_project/minisass_frontend/src/components/ManageImagesModal/index.tsx
+++ django_project/minisass_frontend/src/components/ManageImagesModal/index.tsx
@@ -51,10 +51,8 @@ const ManageImagesModal: React.FC<ManageImageProps> = ({
       filteredImages.forEach((image) => {
         if(image.pest_name.toLowerCase().replace(/\s+/g, '_') !== aiGroup.toLowerCase().replace(/\s+/g, '_')){
           setIsGroupMatching(false)
-          break
         }else {
           setIsGroupMatching(true)
-          break
         }
       });
 
