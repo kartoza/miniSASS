631ea38f5465b0815fce9500392a9220d6715296
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-02-06 11:48:34 +0200

    patch: additional prop for score form

--- django_project/minisass_frontend/src/components/DataInputForm/index.tsx
+++ django_project/minisass_frontend/src/components/DataInputForm/index.tsx
@@ -1381,7 +1381,7 @@ const DataInputForm: React.FC<DataInputFormProps> = (props) => {
         </div>
       </div>
       ): (
-        <ScoreForm onCancel={handleHideScoreForm} additionalData={formValues} setSidebarOpen={props.setSidebarOpen} setProceedToSavingData={setProceedToSavingData} />
+        <ScoreForm onCancel={handleHideScoreForm} additionalData={formValues} setSidebarOpen={props.setSidebarOpen} proceedToSavingData={proceedToSavingData} setProceedToSavingData={setProceedToSavingData} />
       )}
     </>
   );
