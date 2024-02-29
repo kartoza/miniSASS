f07035be77b55134049e8a390659c81fee029c92
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-02-29 09:23:18 +0200

    patch: add arrow icons to custom scroll bar

--- django_project/minisass_frontend/src/components/TabbedContent/index.tsx
+++ django_project/minisass_frontend/src/components/TabbedContent/index.tsx
@@ -36,33 +36,67 @@ const TabbedContent = ({ tabsData, activeTabIndex, onTabChange }) => {
 
 
   return (
-    <div className="flex flex-col items-start justify-start " >
-      <div className="flex items-center">
-        <div className="tabs-container flex gap-5 w-[30vw]" style={{ overflowX: 'auto' }}>
-          {sortedTabsData.map((tab, index) => (
-            <button
-              key={tab.id}
-              className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${activeTabIndex === index ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
-              onClick={() => handleTabChange(index)}
-            >
-              {tab.label}
-            </button>
-          ))}
-        </div>
-        <div
-          className="w-4 h-full bg-gray-200 cursor-pointer"
-          onClick={handleScrollbarClick}
-        ></div>
-      </div>
-      <div className="w-full" style={{marginTop: '-10px'}}>
+  <div className="flex flex-col items-start justify-start">
+    <div className="flex items-center">
+      <div className="tabs-container flex gap-5 w-[30vw]">
         {sortedTabsData.map((tab, index) => (
-          <div key={`content_${tab.id}`} style={{ display: activeTabIndex === index ? 'block' : 'none' }}>
-            {tab.content}
-          </div>
+          <button
+            key={tab.id}
+            className={`cursor-pointer min-w-[126px] text-base text-center focus:outline-none ${activeTabIndex === index ? 'border-b-4 border-blue-900 text-blue-900' : ''}`}
+            onClick={() => handleTabChange(index)}
+          >
+            {tab.label}
+          </button>
         ))}
       </div>
     </div>
-  );
+    <div className="flex items-center mt-2.5 w-[30vw]">
+      <div
+        className="flex justify-between items-center w-full bg-gray-200 rounded-md h-4 cursor-pointer"
+        onClick={handleScrollbarClick}
+      >
+        <div className="w-1/2 h-full flex justify-center items-center text-gray-600">
+          {/* Add left arrow icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 3a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1zM6 9a1 1 0 0 1 1-1h7.586l-3.293-3.293a1 1 0 1 1 1.414-1.414l5 5a1 1 0 0 1 0 1.414l-5 5a1 1 0 1 1-1.414-1.414L13.586 10H7a1 1 0 0 1-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="w-1/2 h-full flex justify-center items-center text-gray-600">
+          {/* Add right arrow icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 3a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1zM6 9a1 1 0 0 1 1-1h7.586l-3.293-3.293a1 1 0 1 1 1.414-1.414l5 5a1 1 0 0 1 0 1.414l-5 5a1 1 0 1 1-1.414-1.414L13.586 10H7a1 1 0 0 1-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div className="w-full">
+      {sortedTabsData.map((tab, index) => (
+        <div key={`content_${tab.id}`} style={{ display: activeTabIndex === index ? 'block' : 'none' }}>
+          {tab.content}
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
 };
 
 export default TabbedContent;
