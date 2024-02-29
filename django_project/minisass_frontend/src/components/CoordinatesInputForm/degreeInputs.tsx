eff8b2c4e00003632f9ed72dd46f951d7e963a8e
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-02-29 14:28:26 +0200

    patch: convert to 6 digits on exit

--- django_project/minisass_frontend/src/components/CoordinatesInputForm/degreeInputs.tsx
+++ django_project/minisass_frontend/src/components/CoordinatesInputForm/degreeInputs.tsx
@@ -39,11 +39,11 @@ function DegreeInput({ label, value, onChange, disabled }: DegreeInputInterface)
     }
   }, [currValue]);
 
-  useEffect(() => {
-    if (value) {
-      convertToSixDecimals(value)
-    }
-  }, [value]);
+  // useEffect(() => {
+  //   if (value) {
+  //     convertToSixDecimals(value)
+  //   }
+  // }, [value]);
 
   return <div
     className="flex sm:flex-col flex-row gap-3 items-center justify-between w-[541px] sm:w-full"
@@ -57,12 +57,9 @@ function DegreeInput({ label, value, onChange, disabled }: DegreeInputInterface)
     <Field
       id={label}
       value={currValue}
-      type="text"
+      type="number"
       onBlur={(evt) => {
-        const value = parseFloat(evt.target.value);
-        if (!isNaN(value)) {
-          setCurrValue(Number(value.toFixed(6)));
-        }
+        convertToSixDecimals(evt.target.value)
       }}
       disabled={disabled}
       className="!placeholder:text-black-900_99 !text-black-900_99 font-raleway md:h-auto p-0 sm:h-auto text-base text-left tracking-[0.50px] w-full"
