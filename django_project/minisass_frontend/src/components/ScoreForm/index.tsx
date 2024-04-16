8cdfd0cff8d05c03790430d1a9d7ca2d303b3daa
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-04-16 20:37:03 +0200

    patch: update error messages

--- django_project/minisass_frontend/src/components/ScoreForm/index.tsx
+++ django_project/minisass_frontend/src/components/ScoreForm/index.tsx
@@ -60,12 +60,10 @@ const ScoreForm: React.FC<ScoreFormProps> = ({ onCancel, additionalData, setSide
                 setButtonStates(response.data.map(score => ({ id: score.id, showManageImages: false })));
                 setIsSavingData(false);
             } else {
-                // Retry after 5 seconds
                 setTimeout(fetchScoreGroups, 5000);
             }
         } catch (error) {
             console.error('Error fetching score groups:', error);
-            // Retry after 5 seconds
             setTimeout(fetchScoreGroups, 5000);
         }
     };
@@ -160,7 +158,7 @@ const ScoreForm: React.FC<ScoreFormProps> = ({ onCancel, additionalData, setSide
       form_data.append('siteId', JSON.stringify(site_id));
       if(saveToExistingSite === true){
         form_data.append('saveToSite', JSON.stringify(true));
-      }
+      }else form_data.append('saveToSite', JSON.stringify(false));
 
       axios.defaults.headers.common['Authorization'] = `Bearer ${state.user.access_token}`;
       const response = await axios.post(
@@ -180,11 +178,14 @@ const ScoreForm: React.FC<ScoreFormProps> = ({ onCancel, additionalData, setSide
         localStorage.setItem('observationId', JSON.stringify(0))
         localStorage.setItem('siteId', JSON.stringify(0))
         if (response.data.status.includes('error')) {
-          setErrorMessage(response.data.message);
           if("Site name already exists" === response.data.message){
             setIsCloseSiteDialogOpen(true);
+          }else {
+            if(response.data.message === "")
+              setErrorMessage("something unexpecttedly went wrong please contact the system administrator via the contact us form.");
+            else setErrorMessage(response.data.message)
+            setIsErrorModalOpen(true);
           }
-          else setIsErrorModalOpen(true);
         }else {
           setProceedToSavingData(false);
           setIsSuccessModalOpen(true);
