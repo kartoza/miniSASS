1f9c5c8bf40458d2a7ddbcba0d74afc696b7c600
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-08-28 12:26:24 +0200

    patch: place banner around slideshow area

--- django_project/minisass_frontend/src/components/SlideShow/index.tsx
+++ django_project/minisass_frontend/src/components/SlideShow/index.tsx
@@ -3,6 +3,7 @@ import { Img } from "../../components/Img";
 import { Fade } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import { globalVariables } from "../../utils";
+import Banner from "../../components/Banner";
 
 // Define an array of images for the slideshow TODO get the images list from the api
 const slideshowImages = [
@@ -13,23 +14,25 @@ const slideshowImages = [
   `${globalVariables.staticPath}img_intrestedcitizensfromduct.png`,
 ];
 
-const Slideshow = () => {
+const Slideshow: React.FC = () => {
   return (
-    <div className="slide-container">
-      <Fade>
-        {slideshowImages.map((fadeImage, index) => (
-          <div key={index}>
-            <Img
-              className="h-[464px] sm:left-[] mt-auto mx-auto object-cover relative rounded-br-[65px] top-10 sm:top-[-80px] md:top-[-85px] w-full mt-[-25px]"
-              src={fadeImage}
-              alt="intrestedcitize"
-            />
-            {/* <h2>{fadeImage.caption}</h2> */}
-          </div>
-        ))}
-      </Fade>
+    <div className="relative">
+      <Banner />
+      <div className="slide-container">
+        <Fade>
+          {slideshowImages.map((fadeImage, index) => (
+            <div key={index}>
+              <Img
+                className="h-[464px] sm:left-[] mt-auto mx-auto object-cover relative rounded-br-[65px] top-10 sm:top-[-80px] md:top-[-85px] w-full mt-[-25px]"
+                src={fadeImage}
+                alt="intrestedcitize"
+              />
+            </div>
+          ))}
+        </Fade>
+      </div>
     </div>
-  )
+  );
 }
 
 
