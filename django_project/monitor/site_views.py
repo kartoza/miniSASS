a9ed90a703c9a9747f76ccf460809dcbdbfdbde5
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-10-03 15:11:59 +0200

    Add swagger docs (#1084)
    * patch: add requirement for swagger
    
    * feat: add documentation configurations
    
    * patch: add urls paths for apis
    
    * feat: add api description for swagger
    
    * patch: show an accurate example
    
    * patch: add api description

--- django_project/monitor/site_views.py
+++ django_project/monitor/site_views.py
@@ -8,6 +8,8 @@ from django.contrib.gis.measure import D
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from minisass.models import GroupScores
 from django.utils.dateparse import parse_date
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 from monitor.serializers import (
@@ -197,6 +199,69 @@ class SiteObservationsByLocation(APIView):
 
 class SitesWithObservationsView(APIView):
 	serializer_class = SitesAndObservationsSerializer
+	@swagger_auto_schema(
+		operation_description="Retrieve detailed information about a site, including its observations and images.",
+		manual_parameters=[
+			openapi.Parameter(
+				'start_date', 
+				openapi.IN_QUERY, 
+				description="Start date in YYYY-MM-DD format", 
+				type=openapi.TYPE_STRING
+			)
+		],
+		responses={
+			200: openapi.Response(
+				description="Successful response",
+				examples={
+					"application/json": {
+						"site": {
+							"gid": 1,
+							"sitename": "Sample Site",
+							"rivername": "Sample River",
+							"rivercategory": "rocky",
+							"sitedescription": "This is a description of the sample site.",
+							"images": [
+								{
+									"id": 1,
+									"image": "https://minisass.org/path/to/image.jpg"
+								},
+								{
+									"id": 2,
+									"image": "https://example.com/path/to/another-image.jpg"
+								}
+							],
+							"observations": [
+								{
+									"obs_id": 123,
+									"obs_date": "2024-08-21",
+									"ml_score": "5.0",
+									"collector_name": "John Doe",
+									"score": 3.45,
+									"comment": "Observation comment",
+									"is_validated": True,
+									"water_clarity": 2.5,
+									"water_temp": 20.3,
+									"ph": 7.2,
+									"diss_oxygen": 8.5,
+									"diss_oxygen_unit": "mgl",
+									"elec_cond": 0.5,
+									"elec_cond_unit": "mS/m",
+									"images": [
+										{
+											"id": 1,
+											"image": "https://example.com/path/to/obs-image.jpg",
+											"description": "Observation image description"
+										}
+									]
+								}
+							]
+						}
+					}
+				}
+			),
+			400: openapi.Response(description="Invalid date format"),
+		}
+	)
 	def get(self, request):
 		start_date_str = request.query_params.get('start_date', None)
 		start_date = parse_date(start_date_str) if start_date_str else None
