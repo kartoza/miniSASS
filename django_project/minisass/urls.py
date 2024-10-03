a1e6f41cf7dadc6f88e372677c4a1fd788dc64a5
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-10-03 11:45:59 +0200

    patch: add urls paths for apis

--- django_project/minisass/urls.py
+++ django_project/minisass/urls.py
@@ -10,8 +10,23 @@ from minisass.views import (
     GetMobileApp,
     get_announcements
 )
-
 from minisass_frontend.views import ReactHomeView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="miniSASS API's",
+      default_version='v1',
+      description="Description of API's",
+      terms_of_service="minisass.org",
+      contact=openapi.Contact(email=""),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 admin.autodiscover()
 
@@ -44,6 +59,10 @@ urlpatterns = [
 
     # google analytics path
     # re_path('djga/', include('google_analytics.urls')),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
