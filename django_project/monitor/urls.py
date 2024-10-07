b99cd01db2f9b605dd7962c314e6137505d62ebb
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-10-07 15:20:17 +0200

    patch: add custom url path for observation image view set (#1090)

--- django_project/monitor/urls.py
+++ django_project/monitor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+# Observation-related views
 from monitor.observation_views import (
     ObservationListCreateView,
     ObservationRetrieveUpdateDeleteView,
@@ -12,6 +13,8 @@ from monitor.observation_views import (
     upload_pest_image,
     delete_pest_image
 )
+
+# Site-related views
 from monitor.site_views import (
     AssessmentListCreateView,
     AssessmentRetrieveUpdateDestroyView,
@@ -22,6 +25,8 @@ from monitor.site_views import (
     SaveObservationImagesView,
     SitesWithObservationsView
 )
+
+# Other general views
 from monitor.views import (
     index,
     wms_get_feature_info,
@@ -46,58 +51,11 @@ router.register(
 
 # URL patterns for the miniSASS app
 urlpatterns = [
-    path('', index, name='monitor_index'),
-    path('wms/~<str:wms_url>~<str:wms_params>', wms_get_feature_info),
+    # Observation Image URLs
     path(
-        'sites/is-land/<str:lat>/<str:long>/',
-        CheckSiteIsLand.as_view(),
-        name='check-coordinate-is-land'
-    ),
-    path('sites/<str:x>/<str:y>/<str:d>/', get_sites),
-    path('closest_site/<str:x>/<str:y>/<str:d>/', get_closest_site),
-    path('unique/<str:field>/', get_unique),
-    path('legacy/observation/<int:obs_id>/', zoom_observation),
-    path('legacy/observations/<int:site_id>/', get_observations),
-    path('observations/download/<int:site_id>/', download_observations),
-    path('observations/download/filtered/~<str:filter_string>',
-         download_observations_filtered),
-    path(
-        'observations/download-v2/<int:site_id>/',
-        DownloadObservations.as_view(),
-        name='download-observations'
-    ),
-    path(
-        'observations/download/filtered/~<str:filter_string>',
-        download_observations_filtered
-    ),
-    path('schools/', get_schools),
-    path('<int:monitor_id>/', detail, name='monitor_detail'),
-
-    path(
-        'observations/',
-        ObservationListCreateView.as_view(),
-        name='observation-list-create'
-    ),
-    path(
-        'observations/<int:pk>/',
-        ObservationRetrieveUpdateDeleteView.as_view(),
-        name='observation-retrieve-update-delete'
-    ),
-
-    path(
-        'sites/',
-        SitesListCreateView.as_view(),
-        name='sites-list-create'
-    ),
-    path(
-        'sites/<int:pk>/',
-        SiteRetrieveUpdateDestroyView.as_view(),
-        name='site-retrieve-update-destroy'
-    ),
-    path(
-        'observations-create/',
-        create_observations,
-        name='create_observations'
+        'observations/<int:observation_pk>/images/', 
+        ObservationImageViewSet.as_view({'get': 'list'}), 
+        name='observation-image-view-list'
     ),
     path(
         'upload-pest-images/', 
@@ -110,29 +68,88 @@ urlpatterns = [
         name='remove-pest-image'
     ),
 
+    # General observation URLs
+    path(
+        'observations/',
+        ObservationListCreateView.as_view(),
+        name='observation-list-create'
+    ),
+    path(
+        'observations/<int:pk>/',
+        ObservationRetrieveUpdateDeleteView.as_view(),
+        name='observation-retrieve-update-delete'
+    ),
     path(
         'observations/recent-observations/',
         RecentObservationListView.as_view(),
         name='recent-observation-list'
     ),
-
     path(
         'observations/observation-details/<int:pk>/',
         ObservationRetrieveView.as_view(),
         name='observation-details'
     ),
+    path(
+        'observations-create/',
+        create_observations,
+        name='create_observations'
+    ),
+    path(
+        'observations/download/<int:site_id>/', 
+        download_observations
+    ),
+    path(
+        'observations/download/filtered/~<str:filter_string>', 
+        download_observations_filtered
+    ),
+    path(
+        'observations/download-v2/<int:site_id>/', 
+        DownloadObservations.as_view(),
+        name='download-observations'
+    ),
+    path(
+        'observations/by-site/<int:site_id>/', 
+        get_observations_by_site,
+        name='observations-by-site'
+    ),
+    path(
+        'observations/<int:observationId>/save-images/', 
+        SaveObservationImagesView.as_view(), 
+        name='save_observation_images'
+    ),
+    path(
+        'observations/observation-details/<int:observation_pk>/', 
+        include(router.urls)
+    ),
 
+    # Site-related URLs
+    path(
+        'sites/',
+        SitesListCreateView.as_view(),
+        name='sites-list-create'
+    ),
+    path(
+        'sites/<int:pk>/',
+        SiteRetrieveUpdateDestroyView.as_view(),
+        name='site-retrieve-update-destroy'
+    ),
+    path(
+        'sites/<int:site_id>/save-images/', 
+        SaveSiteImagesView.as_view(), 
+        name='save_site_images'
+    ),
+    path(
+        'sites-with-observations/', 
+        SitesWithObservationsView.as_view(), 
+        name='sites-with-observations'
+    ),
     path(
         'site-observations/<latitude>/<longitude>/', 
         SiteObservationsByLocation.as_view(), 
         name='site-observations'
     ),
 
-    path(
-        'observations/observation-details/<int:observation_pk>/',
-        include(router.urls)
-    ),
-
+    # Site assessment URLs
     path(
         'site-assessments/', 
         AssessmentListCreateView.as_view(), 
@@ -143,24 +160,20 @@ urlpatterns = [
         AssessmentRetrieveUpdateDestroyView.as_view(), 
         name='assessment-retrieve-update-destroy'
     ),
+
+    # Other functionality
+    path('', index, name='monitor_index'),
+    path('wms/~<str:wms_url>~<str:wms_params>', wms_get_feature_info),
     path(
-        'observations/by-site/<int:site_id>/', 
-        get_observations_by_site,
-        name='observations-by-site'
+        'sites/is-land/<str:lat>/<str:long>/', 
+        CheckSiteIsLand.as_view(), 
+        name='check-coordinate-is-land'
     ),
-    path(
-        'sites/<int:site_id>/save-images/', 
-        SaveSiteImagesView.as_view(), 
-        name='save_site_images'
-    ),
-    path(
-        'observations/<int:observationId>/save-images/', 
-        SaveObservationImagesView.as_view(), 
-        name='save_observation_images'
-    ),
-    path(
-        'sites-with-observations/', 
-        SitesWithObservationsView.as_view(), 
-        name='sites-with-observations'
-    ),
+    path('sites/<str:x>/<str:y>/<str:d>/', get_sites),
+    path('closest_site/<str:x>/<str:y>/<str:d>/', get_closest_site),
+    path('unique/<str:field>/', get_unique),
+    path('legacy/observation/<int:obs_id>/', zoom_observation),
+    path('legacy/observations/<int:site_id>/', get_observations),
+    path('schools/', get_schools),
+    path('<int:monitor_id>/', detail, name='monitor_detail'),
 ]
