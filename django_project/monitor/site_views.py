4c4be4d6091d039f872d1c842fd7d0fc4dfc6900
Tinashe <70011086+tinashechiraya@users.noreply.github.com>
2024-10-09 11:52:20 +0200

    patch: use token to authenticate aganist API

--- django_project/monitor/site_views.py
+++ django_project/monitor/site_views.py
@@ -20,6 +20,7 @@ from minisass.models import GroupScores
 from django.utils.dateparse import parse_date
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from minisass_authentication.permissions import IsAuthenticatedOrWhitelisted
 
 
 from monitor.serializers import (
@@ -203,6 +204,7 @@ class SiteObservationsByLocation(APIView):
 
 class SitesWithObservationsView(APIView):
 	serializer_class = SitesAndObservationsSerializer
+	permission_classes = [IsAuthenticatedOrWhitelisted]
 	@swagger_auto_schema(
 		operation_description="Retrieve detailed information about a site, including its observations and images.",
 		manual_parameters=[
