e8c40530cfdd90273c24163fee24d30ff687cbbf
Zulfikar Akbar Muzakki <zakki@kartoza.com>
2025-06-17 12:53:34 +0700

    Fix logout (#1201)
    * Fix logout
    
    * Update views.py
    
    ---------
    
    Co-authored-by: Dimas Ciputra <dimas@kartoza.com>

--- django_project/minisass_authentication/views.py
+++ django_project/minisass_authentication/views.py
@@ -20,14 +20,13 @@ from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import api_view
 from rest_framework.decorators import (
 	api_view,
 	permission_classes
 )
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.response import Response
