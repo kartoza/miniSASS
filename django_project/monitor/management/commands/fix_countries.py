eada7bd270d4e0f36a68042385720aa2db0be2d6
Z.A. Muzakki <zakki@kartoza.com>
2025-09-10 13:10:41 +0700

    Add comments

--- django_project/monitor/management/commands/fix_countries.py
+++ django_project/monitor/management/commands/fix_countries.py
@@ -19,6 +19,7 @@ class Command(BaseCommand):
     help = 'Fix site and user countries'
 
     def handle(self, *args, **kwargs):
+        # Fix Site countries
         for site in Sites.objects.all():
             # if site has no country, set it
             if not site.country:
@@ -40,6 +41,7 @@ class Command(BaseCommand):
                     logger.error('Error fixing User country %s: %s', site.user, e)
                 logger.info('Site %s fixed successfully', site.gid)
 
+        # Fix User countries
         correct_country(None, None, User.objects.all())
 
         logger.info('Finished fixing countries')
